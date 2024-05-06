6540cc3d292f2f7666d036aa14a2aeb4886b2dc1
Juan Sebastian Calderon <juan.calderon02@unicatolica.edu.co>
2024-05-06 17:50:43 -0500

    Update Vista.jsx

--- frontend/src/assets/components/vuelos/vista.jsx
+++ frontend/src/assets/components/vuelos/vista.jsx
@@ -13,7 +13,7 @@ const TicketForm = () => {
 
   useEffect(() => {
     if (origin) {
-      fetch(`https://backend-pied-one.vercel.app/api/vuelos/paises?query=${origin}`)
+      fetch(`http://localhost:3000/api/vuelos/paises?query=${origin}`)
         .then(response => response.json())
         .then(data => {
           console.log(data); // Muestra los datos recibidos en la consola
@@ -25,7 +25,7 @@ const TicketForm = () => {
 
   useEffect(() => {
     if (destination) {
-      fetch(`https://backend-pied-one.vercel.app/api/vuelos/paises?query=${destination}`)
+      fetch(`http://localhost:3000/api/vuelos/paises?query=${destination}`)
         .then(response => response.json())
         .then(data => {
           console.log(data); // Muestra los datos recibidos en la consola
@@ -41,7 +41,7 @@ const handleSaveTicket = async () => {
       // Obtener la fecha y hora actual
       const fechaActual = new Date();
 
-      const response = await fetch('https://backend-pied-one.vercel.app/api/vuelos/boletos', {
+      const response = await fetch('http://localhost:3000/api/vuelos/boletos', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
